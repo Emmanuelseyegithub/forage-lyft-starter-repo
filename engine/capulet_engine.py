1558b515ad82481c9e8f38eea1c7b36d4fbd5936
Emmanuel. O <78210104+Emmanuelseyegithub@users.noreply.github.com>
2024-02-01 10:34:52 +0000

    Update capulet_engine.py

--- engine/capulet_engine.py
+++ engine/capulet_engine.py
@@ -1,9 +1,9 @@
 from abc import ABC
 
-from car import Car
+from car.car import Car
 
 
-class CapuletEngine(Car, ABC):
+class CapuletEngine(Car):
     def __init__(self, last_service_date, current_mileage, last_service_mileage):
         super().__init__(last_service_date)
         self.current_mileage = current_mileage
