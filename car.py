ebf76e62f6da43f4cb6ccb26400cce53d6345c5a
Emmanuel. O <78210104+Emmanuelseyegithub@users.noreply.github.com>
2024-02-01 10:43:13 +0000

    Update car.py

--- car.py
+++ car.py
@@ -1,10 +1,11 @@
-from abc import ABC, abstractmethod
+from serviceable import serviceable
 
 
-class Car(ABC):
-    def __init__(self, last_service_date):
-        self.last_service_date = last_service_date
+class Car(serviceable):
+    def __init__(self, engine, battery):
+        self.engine = engine
+        self.battery = battery
 
-    @abstractmethod
     def needs_service(self):
-        pass
+        return self.engine.needs_service() or self.battery.needs_service()
+    
